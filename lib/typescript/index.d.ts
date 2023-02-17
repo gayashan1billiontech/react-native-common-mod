95f0f52720046875d8e7075fb09d2585154c7aa3
gayashan1billiontech <gayashan.galagedara@1billiontech.com>
2023-02-17 11:06:33 +0530

    first commit

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,3 @@
+import App from './App';
+export { App };
+//# sourceMappingURL=index.d.ts.map
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App(props: any) {
+  const [result, setResult] = React.useState<number | undefined>();
+
+  React.useEffect(() => {
+    setResult(props.name);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
